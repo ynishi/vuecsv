e9c65bc9fe30ec13afb0f408833a6313644c15ed
Yutaka Nishimura <ytk.nishimura@gmail.com>
2018-01-15 23:52:18 +0900

    add props test case for CsvDownload

--- test/unit/specs/CsvDownload.spec.js
+++ test/unit/specs/CsvDownload.spec.js
@@ -2,27 +2,40 @@ import Vue from 'vue'
 import CsvDownload from '@/components/CsvDownload'
 import * as td from 'testdouble'
 
+function getRendered (Component, propsData) {
+  const Constructor = Vue.extend(Component)
+  const vm = new Constructor({ propsData: propsData }).$mount()
+  return vm
+}
+
 describe('CsvDownload.vue', () => {
   it('should render CsvDownload Component', () => {
-    const Constructor = Vue.extend(CsvDownload)
-    const vm = new Constructor().$mount()
+    const vm = getRendered(CsvDownload, {})
     expect(vm.$el.className).to.equal('csvdownload')
   })
-  it('should convert csv', () => {
-    const Constructor = Vue.extend(CsvDownload)
-    const vm = new Constructor().$mount()
+  it('should render with props', () => {
     const testDataJson = [{'a': 1, 'b': 2}, {'a': 3, 'b': 4}]
-
-    vm.dataJson = testDataJson
+    const propsData = {
+      title: 'testTitle',
+      filename: 'testFile.csv',
+      convOption: { opt1: 'testOpt' },
+      dataJson: testDataJson
+    }
+    const vm = getRendered(CsvDownload, propsData)
+    expect(vm.title).to.equal('testTitle')
+    expect(vm.filename).to.equal('testFile.csv')
+    expect(vm.convOption.opt1).to.equal('testOpt')
+    expect(vm.dataJson).to.equal(testDataJson)
+  })
+  it('should convert csv', () => {
+    const testDataJson = [{'a': 1, 'b': 2}, {'a': 3, 'b': 4}]
+    const vm = getRendered(CsvDownload, { dataJson: testDataJson })
     vm.convert()
-
     const expected = 'a,b\r\n1,2\r\n3,4'
-
     expect(expected).to.equal(vm.dataCSV)
   })
   it('should called download', () => {
-    const Constructor = Vue.extend(CsvDownload)
-    const vm = new Constructor().$mount()
+    const vm = getRendered(CsvDownload, {})
 
     var download = td.function('vm.download')
     download()
