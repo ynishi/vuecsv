d859b08f0c87eb6e57fd73c9507c0b4d51671aa5
Yutaka Nishimura <ytk.nishimura@gmail.com>
2018-01-18 01:10:59 +0900

    update test case of CsvDownload to support header

--- test/unit/specs/CsvDownload.spec.js
+++ test/unit/specs/CsvDownload.spec.js
@@ -8,13 +8,28 @@ function getRendered (Component, propsData) {
   return vm
 }
 
+const testHeader = {
+        'a': 'headerA',
+        'b': 'headerB'
+        }
+
+const testDataJson = [
+  {
+    'a': 1,
+    'b': 2
+  },
+  {
+    'a': 3,
+    'b': 4
+  }
+]
+
 describe('CsvDownload.vue', () => {
   it('should render CsvDownload Component', () => {
     const vm = getRendered(CsvDownload, {})
     expect(vm.$el.className).to.equal('csvdownload')
   })
   it('should render with props', () => {
-    const testDataJson = [{'a': 1, 'b': 2}, {'a': 3, 'b': 4}]
     const propsData = {
       title: 'testTitle',
       filename: 'testFile.csv',
@@ -28,10 +43,15 @@ describe('CsvDownload.vue', () => {
     expect(vm.dataJson).to.equal(testDataJson)
   })
   it('should convert csv', () => {
-    const testDataJson = [{'a': 1, 'b': 2}, {'a': 3, 'b': 4}]
     const vm = getRendered(CsvDownload, { dataJson: testDataJson })
     vm.convert()
     const expected = 'a,b\r\n1,2\r\n3,4'
+    expect(expected).to.equal(vm.dataCSV)
+  })
+  it('should convert csv with header', () => {
+    const vm = getRendered(CsvDownload, { header: testHeader, dataJson: testDataJson })
+    vm.convert()
+    const expected = 'headerA,headerB\r\n1,2\r\n3,4'
     expect(expected).to.equal(vm.dataCSV)
   })
   it('should called download', () => {
