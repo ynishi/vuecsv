adf390f08d97d3733f375ae0e3c162422bdd6b0c
Yutaka Nishimura <ytk.nishimura@gmail.com>
2018-01-20 21:02:06 +0900

    update index.js support option

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import CsvDownload from './components/CsvDownload.vue'
+import CsvOption from './components/CsvOption.vue'
 
 function plugin (Vue) {
   Vue.component(CsvDownload.name, CsvDownload)
+  Vue.component(CsvOption.name, CsvOption)
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
@@ -13,5 +15,6 @@ const version = '__VERSION__'
 
 export {
   CsvDownload,
+  CsvOption,
   version
 }
